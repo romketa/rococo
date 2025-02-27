58cd679a0e82608ac739ca4d88f9c6da0be8dffb
romketa <mr@romketa.ru>
2025-02-27 19:56:17 +0400

    Add csrf to Security config

--- rococo-gateway/src/main/java/guru/qa/rococo/config/SecurityConfig.java
+++ rococo-gateway/src/main/java/guru/qa/rococo/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -29,6 +30,7 @@ public class SecurityConfig {
     corsCustomizer.corsCustomizer(http);
 
     http
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(customizer ->
         customizer
             .requestMatchers(
